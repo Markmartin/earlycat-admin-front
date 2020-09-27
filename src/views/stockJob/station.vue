dc837770c19c6c2792c28d300015c9e4a99dbeae
hu-1996 <781531711@qq.com>
2020-09-27 20:23:46 +0800

    新增工位配货

--- src/views/stockJob/station.vue
+++ src/views/stockJob/station.vue
@@ -2,8 +2,18 @@
   <div class="app-container">
     <el-form :inline="true" :model="params" class="demo-form-inline">
       <el-form-item label="分类">
-        <el-select v-model="params.stationId" filterable placeholder="请选择" clearable>
-          <el-option v-for="item in stations" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        <el-select
+          v-model="params.stationId"
+          filterable
+          placeholder="请选择"
+          clearable
+        >
+          <el-option
+            v-for="item in stations"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="日期">
@@ -14,27 +24,39 @@
           value-format="yyyy-MM-dd"
         ></el-date-picker>
       </el-form-item>
-      <el-button type="primary" @click="stationOrder" v-permission="['GET /admin/stockJob/stationOrder']">查询</el-button>
+      <el-button
+        type="primary"
+        @click="stationOrder"
+        v-permission="['GET /admin/stockJob/stationOrder']"
+        >查询</el-button
+      >
       <el-button type="primary" @click="CreateOneFormPage">打印</el-button>
     </el-form>
 
     <el-table
       :data="list"
-      style="width: 100%;"
+      style="width: 100%"
       border
       id="printJS-formOrder"
-      :row-style="{height: '1px'}"
-      :cell-style="{padding: '5px'}"
+      :row-style="{ height: '1px' }"
+      :cell-style="{ padding: '5px' }"
     >
-      <el-table-column align="center" label="序号" width="70" type="index"></el-table-column>
-      <el-table-column label="库位" width="80">
+      <el-table-column
+        align="center"
+        label="序号"
+        width="70"
+        type="index"
+      ></el-table-column>
+      <el-table-column label="库位" width="100">
         <template slot-scope="scope">{{ scope.row.stationName }}</template>
       </el-table-column>
       <el-table-column label="产品名称" width="300">
         <template slot-scope="scope">{{ scope.row.goodsName }}</template>
       </el-table-column>
       <el-table-column align="center" label="规格" width="120">
-        <template slot-scope="scope">{{ scope.row.specifications | spec }}</template>
+        <template slot-scope="scope">{{
+          scope.row.specifications | spec
+        }}</template>
       </el-table-column>
       <el-table-column align="center" label="数量" width="80">
         <template slot-scope="scope">{{ scope.row.number }}</template>
@@ -54,6 +76,7 @@ export default {
   data() {
     return {
       list: [],
+      list_bak:[],
       params: {
         date: formatDate(
           new Date(new Date().setDate(new Date().getDate() - 1))
@@ -65,19 +88,22 @@ export default {
   created() {
     this.stationOrder();
     this.stationList();
-    var str = '["箱"]';
-    var arrEval = eval(str);
-    console.log(arrEval[0]);
   },
-  filters :{
-      spec(value) {
-          return eval(value)[0];
-      }
+  filters: {
+    spec(value) {
+      return eval(value)[0];
+    },
   },
   methods: {
     async stationOrder() {
-      const res = await stationOrder(this.params);
-      this.list = res.data.data;
+      if (this.params.stationId) {
+          let data = JSON.parse(JSON.stringify(this.list_bak))
+          this.list = data.filter(f => f.stationId == this.params.stationId)
+      } else {
+        const res = await stationOrder(this.params);
+        this.list = res.data.data;
+        this.list_bak = res.data.data;
+      }
     },
     async stationList() {
       const res = await stationList();
@@ -85,9 +111,9 @@ export default {
     },
     CreateOneFormPage() {
       print({
-        printable: "printJS-formPur",
+        printable: "printJS-formOrder",
         type: "html",
-        header: "订单汇总",
+        header: "工位配货",
         headerStyle: "text-align:center;color:#f00;width:100%;",
         // targetStyles: ['border', 'padding: 15px'],
         scanStyles: false,
